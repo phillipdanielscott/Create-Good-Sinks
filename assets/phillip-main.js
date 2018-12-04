97e3b6c3f408e30f5c81ef1237a7093870fcd5c3
Phillip  Scott <phillipd20@yahoo.com>
2018-12-04 11:50:30 -0500

    added owl carousel

---
+++ assets/phillip-main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  // console.log("js file working");
+  // $(".owl-carousel").owlCarousel();
+// 
+//   if($.isFunction('owlCarousel')){
+//   $(".owl-demo").owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+//   });
+// }
+});
