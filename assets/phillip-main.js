da7e11b16e005ddef473efd9c238a1c8eb970e11
Phillip  Scott <phillipd20@yahoo.com>
2019-01-07 16:34:36 -0500

    got the collapsible div working

--- assets/phillip-main.js
+++ assets/phillip-main.js
@@ -1,23 +1,62 @@
-$(document).ready(function(){
-  // console.log("js file working");
-  // $(".owl-carousel").owlCarousel();
-// 
-//   if($.isFunction('owlCarousel')){
-//   $(".owl-demo").owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:5
-//         }
-//     }
-//   });
-// }
+$(document).ready(function() {
+  function initCollapsibleWithjQuery() {
+    var collapsibleButtons$ = $(".collapsible");
+    collapsibleButtons$.each(function(index, ele) {
+       var collapsible$ = $(ele),
+           content$ = collapsible$.next();
+       collapsible$.click(function() {
+         collapsible$.toggleClass("active");
+         if (!collapsible$.hasClass("active")) {
+           content$.css("maxHeight", "0px");
+         } else {
+           content$.css("maxHeight", content$.prop("scrollHeight")+"px");
+         }
+       });
+    });
+  }
+
+ /************************************************************************
+  * Create a collapsible section. When a user clicks on a collapsible the element immediately
+  * after it will toggle being open and closed.
+  * @param collapsibles$ [$(".collapsible")] jQuery DOM object to make collapsible. This is the "trigger" part.
+  */
+ function initializeCollapsible(collapsibles$) {
+   if (collapsibles$ === null) {
+     var collapsibles$ = $(".collapsible");
+   }
+
+  collapsibles$.each(function(index, ele) {
+       var collapsible$ = $(ele),
+           content$ = collapsible$.next();
+       collapsible$.click(function() {
+         collapsible$.toggleClass("active");
+         if (collapsible$.hasClass("active")) {
+           content$.css("maxHeight", content$.prop("scrollHeight")+"px");
+         } else {
+           content$.css("maxHeight", "0px");
+         }
+       });
+    });
+ }
+
+  function initCollapsibleWithVanillaJs() {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
+
+  }
+
+ initCollapsibleWithjQuery();
+ // initCollapsibleWithVanillaJs();
 });
